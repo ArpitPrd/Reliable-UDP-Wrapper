e4f7a4ca8b40a452585e0c801d2ab3ecf5265bab
ArpitPrd <prd.arpit@gmail.com>
2025-11-05 11:13:11 +0530

    part2

--- part2/p2_server.py
+++ part2/p2_server.py
@@ -91,8 +91,8 @@ class Server:
         self.ack_credits = 0.0
 
         # CUBIC params (still used as loss fallback)
-        self.C = 0.4
-        self.beta_cubic = 0.7
+        self.C = 1.0
+        self.beta_cubic = 0.9
         self.w_max_bytes = 0.0
         self.w_max_last_bytes = 0.0
         self.t_last_congestion = 0.0
